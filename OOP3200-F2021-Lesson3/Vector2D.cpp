ddb4058749e6e0eb2256270c24174eacbbb16ef4
shweta patel <Shwetapatel0810@outlook.com>
2021-09-20 09:17:21 -0400

    Add project files.

---
+++ OOP3200-F2021-Lesson3/Vector2D.cpp
@@ -0,0 +1,28 @@
+
+#ifndef __VECTOR_2D__
+#define __VECTOR_2D__
+
+class Vector2D
+{
+public:
+	//CONSTRUCTOR(2)
+
+	//DESTRUCTOR
+
+	//GETTERS (Accessors)
+	
+	//SETTERS (Mutators)
+
+	//public method (class Function )
+
+private:
+
+	//INSTANCE VARIABLE (Class Member Variable)
+
+	//PRIVATE METHDOS (Private Class )
+
+};
+//#include "Vector2D.h"
+
+
+#endif // !__VECTOR2D__
---
+++ OOP3200-F2021-Lesson3/main.cpp
@@ -0,0 +1,15 @@
+/*
+* Author : Shweta Patel
+* Student ID: 100773663
+* 
+* Date : September 20, 2021
+* 
+* Description: Demo project for OOP3200- F2021- Week3. 
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
